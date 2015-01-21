93dd55298d916801444c6135d852eeea3bc5e2a8
huyanping <hypxm@qq.com>
2015-01-21 22:09:49 +0800

    update

---
+++ tests/simple.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jenner
+ * Date: 15-1-21
+ * Time: 下午10:07
+ */
+
+
+
+$records = array(
+    array(
+        "state" => "IN",
+        "city" => "Indianapolis",
+        "object" => "School bus"
+    ),
+    array(
+        "state" => "IN",
+        "city" => "Indianapolis",
+        "object" => "Manhole"
+    ),
+    array(
+        "state" => "IN",
+        "city" => "Plainfield",
+        "object" => "Basketball"
+    ),
+    array(
+        "state" => "CA",
+        "city" => "San Diego",
+        "object" => "Light bulb"
+    ),
+    array(
+        "state" => "CA",
+        "city" => "Mountain View",
+        "object" => "Space pen"
+    )
+);
+
+$group_by_fields = [
+    'state' => function($value){
+            return substr($value, 0, 1);
+        }
+];
+
+$group_by_value = [
+    'state', 'object',
+    'city' => function($value){
+            return count($value);
+        }
+];
+
+$grouped = \Jenner\Zebra\ArrayGroupBy::groupBy($records, $group_by_fields, $group_by_value);
+print_r($grouped);
